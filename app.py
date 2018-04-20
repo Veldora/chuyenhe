51b83285f9f20a1ba3b081c123c82bc9be83dbc9
DESKTOP-DVMGHEA\Veldora Tempest <minh101202@gmail.com>
2018-04-21 01:10:36 +0700

    no message

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index2():
+    return render_template('chuyenhe.html')
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
