2c951f5969f0aef4c2ae5e96038390730b716b81
DESKTOP-DVMGHEA\Veldora Tempest <minh101202@gmail.com>
2018-04-26 14:19:12 +0700

    re

--- app.py
+++ app.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def index2():
     return render_template('chuyenhe.html')
 
-@app.route('/google71f185714e0c0e1a')
-def index2():
-    return render_template('google71f185714e0c0e1a.html')
-
 if __name__ == '__main__':
   app.run(debug=True)
